062b45d19ea47198791668fb6df22b81c3abbb9b
João Victor <joaovictorramalho7@gmail.com>
2021-10-14 23:19:43 -0300

    Remove importações desnecessárias

--- mobile/src/pages/Activity/index.tsx
+++ mobile/src/pages/Activity/index.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 
-import { StyleSheet, View, ScrollView } from 'react-native';
-
-import { Command } from '../../components/Command';
+import { ScrollView } from 'react-native';
 
 import {
 	Description,
@@ -298,49 +296,3 @@ export function Activity() {
 		</Container>
 	)
 }
-
-const styles = StyleSheet.create({
-	Top: {
-		flexDirection: 'row',
-		height: 20,
-		backgroundColor: '#1C2124',
-		borderTopLeftRadius: 8,
-		borderTopRightRadius: 8,
-	},
-	red: {
-		marginLeft: 16,
-		marginTop: 3,
-		width: 14,
-		height: 14,
-		borderRadius: 50,
-		backgroundColor: '#FF5A54',
-	},
-	yellow: {
-		marginLeft: 4,
-		marginTop: 3,
-		width: 14,
-		height: 14,
-		borderRadius: 50,
-		backgroundColor: '#E5BF2F',
-	},
-	green: {
-		marginLeft: 4,
-		marginTop: 3,
-		width: 14,
-		height: 14,
-		borderRadius: 50,
-		backgroundColor: '#51C22D',
-	},
-	bottom: {
-		minHeight: 120,
-		backgroundColor: '#000000',
-		borderBottomLeftRadius: 8,
-		borderBottomRightRadius: 8,
-	},
-	arrow: {
-		marginTop: 8,
-		marginLeft: 4,
-		color: '#008000',
-		fontSize: 24,
-	}
-});
