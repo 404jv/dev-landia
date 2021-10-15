dc576509d6a6fe2cd8ab2c1c3c8d91b7b23549d5
João Victor <joaovictorramalho7@gmail.com>
2021-10-14 23:18:30 -0300

    Estilização de todo o Bash pelo stlyed-components

--- mobile/src/components/Bash/index.tsx
+++ mobile/src/components/Bash/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View } from 'react-native';
 import { Command } from '../Command';
 
-import { BashContainer, BashContent } from './styles';
+import { Arrow, BashContent, Body, Circle, Header } from './styles';
 
 interface IOption {
   name: string,
@@ -18,67 +18,22 @@ interface IBashProps {
 export function Bash({ options }: IBashProps) {
 
   return (
-    <BashContainer>
-      <View style={styles.Top}>
-        <View style={styles.red} />
-        <View style={styles.yellow} />
-        <View style={styles.green} />
-      </View>
-      <View style={styles.bottom}>
-        <Text style={styles.arrow}> {'>'} </Text>
+    <View>
+      <Header>
+    	<Circle style={{ marginLeft: 16, backgroundColor: '#FF5A54' }} />
+        <Circle style={{ backgroundColor: '#E5BF2F' }} />
+        <Circle style={{ backgroundColor: '#51C22D' }} />
+      </Header>
+
+      <Body>
+        <Arrow> {'>'} </Arrow>
 
         <BashContent>
           {options.map((line, index) => (
             <Command key={index} commandName={line.name} />
           ))}
         </BashContent>
-      </View>
-    </BashContainer>
+      </Body>
+    </View>
   )
 }
-
-const styles = StyleSheet.create({
-	Top: {
-		flexDirection: 'row',
-		height: 20,
-		backgroundColor: '#1C2124',
-		borderTopLeftRadius: 8,
-		borderTopRightRadius: 8,
-	},
-	red: {
-		marginLeft: 16,
-		marginTop: 3,
-		width: 14,
-		height: 14,
-		borderRadius: 50,
-		backgroundColor: '#FF5A54',
-	},
-	yellow: {
-		marginLeft: 4,
-		marginTop: 3,
-		width: 14,
-		height: 14,
-		borderRadius: 50,
-		backgroundColor: '#E5BF2F',
-	},
-	green: {
-		marginLeft: 4,
-		marginTop: 3,
-		width: 14,
-		height: 14,
-		borderRadius: 50,
-		backgroundColor: '#51C22D',
-	},
-	bottom: {
-		minHeight: 120,
-		backgroundColor: '#000000',
-		borderBottomLeftRadius: 8,
-		borderBottomRightRadius: 8,
-	},
-	arrow: {
-		marginTop: 8,
-		marginLeft: 4,
-		color: '#008000',
-		fontSize: 24,
-	}
-});
--- mobile/src/components/Bash/styles.ts
+++ mobile/src/components/Bash/styles.ts
@@ -1,11 +1,36 @@
 import styled from 'styled-components/native';
 
-export const BashContainer = styled.View`
-
+export const BashContent = styled.View`
+  flex-direction: row;
+  margin-left: 16px;
+  flex-wrap: wrap;
 `;
 
-export const BashContent = styled.View`
-    flex-direction: row;
-    margin-left: 16px;
-    flex-wrap: wrap;
+export const Circle = styled.View`
+	margin-left: 4px;
+	margin-top: 3px;
+	width: 14px;
+	height: 14px;
+	border-radius: 50px;
+`;
+
+export const Header = styled.View`
+	flex-direction: row;
+	height: 20px;
+	background-color: #1C2124;
+	border-top-left-radius: 8px;
+	border-top-right-radius: 8px;
+`;
+
+export const Body = styled.View`
+	min-height: 120px;
+	background-color: #000000;
+	border-bottom-left-radius: 8px;
+	border-bottom-right-radius: 8px;
+`;
+
+export const Arrow = styled.Text`
+	margin-left: 4px;
+	color: #008000;
+  font-size: 24px;
 `;
