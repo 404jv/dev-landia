4d6621e964a4f9bf1bbe0cb22747fbe435aa6663
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2021-10-28 19:14:42 -0300

    Cria função para tocar sons

---
+++ mobile/src/utils/playSound.ts
@@ -0,0 +1,12 @@
+import { Audio } from 'expo-av';
+
+export async function playSound(songName: string) {
+  const songsPath = {
+    correctSong: require('../assets/correctSong.mp3'),
+    wrongSong: require('../assets/wrongSong.mp3'),
+    finishedGame: require('../assets/finishedGame.mp3'),
+  }
+
+  const { sound } = await Audio.Sound.createAsync(songsPath[songName])
+  await sound.playAsync();
+}
