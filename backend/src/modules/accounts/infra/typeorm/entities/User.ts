85ff3dede5274f6cc147753fbb62f8794fd4f439
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-23 16:05:30 -0300

    Create entity user

---
+++ backend/src/modules/accounts/infra/typeorm/entities/User.ts
@@ -0,0 +1,55 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  is_admin: boolean;
+
+  @Column()
+  password: string;
+
+  @Column()
+  avatar_name?: string;
+
+  @Column()
+  biography?: string;
+
+  @Column()
+  total_xp?: number;
+
+  @Column()
+  total_coins?: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User };
