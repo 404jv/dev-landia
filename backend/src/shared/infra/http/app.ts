5a090c2bfd2b57392fc6b48ec91f4ace6c3aea44
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-23 21:52:23 -0300

    Add middleware to return errors

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -1,9 +1,13 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+
+import { IUseCaseError } from '@core/domain/errors/IUseCaseError';
 
 import createConnection from '../typeorm/index';
 import { router } from './routes';
+
 import '@shared/container';
 
 createConnection().then(() => console.log('📦 Database connected.'));
@@ -12,5 +16,19 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (
+    error: IUseCaseError,
+    _request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    return response.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+);
 
 export { app };
