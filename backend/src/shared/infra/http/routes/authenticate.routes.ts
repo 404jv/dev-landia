3277e226d0a58ef97c8693b4c9ead7b4ec95970a
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-24 22:53:12 -0300

    Create routes to authenticate

---
+++ backend/src/shared/infra/http/routes/authenticate.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import { AuthenticateUserController } from '@modules/accounts/useCases/AuthenticateUser/AuthenticateUserController';
+
+const authenticateRoutes = Router();
+
+const authenticateUserController = new AuthenticateUserController();
+
+authenticateRoutes.post('/sessions', authenticateUserController.handle);
+
+export { authenticateRoutes };
