c14e005de675919c6ace39df65e6cbd14cb8cd1b
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-28 14:13:28 -0300

    Add singleton to MapsRepositroy

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -2,8 +2,12 @@ import { container } from 'tsyringe';
 
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { MapsRepository } from '@modules/maps/infra/typeorm/repositories/MapsRepository';
+import { IMapsRepository } from '@modules/maps/repositories/IMapsRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
 );
+
+container.registerSingleton<IMapsRepository>('MapsRepository', MapsRepository);
