370ca3b4cc8114a4dc75bf5b6902a544ca2b5dad
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-28 14:51:50 -0300

    Apply ensureAuthenticated to all maps routes and add ensureAdmin to route to create map

--- backend/src/shared/infra/http/routes/maps.routes.ts
+++ backend/src/shared/infra/http/routes/maps.routes.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 
 import { CreateMapController } from '@modules/maps/useCases/CreateMap/CreateMapController';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const mapsRoutes = Router();
 
 const createMapController = new CreateMapController();
+mapsRoutes.use(ensureAuthenticated);
 
-mapsRoutes.post('/create', ensureAuthenticated, createMapController.handle);
+mapsRoutes.post('/create', ensureAdmin, createMapController.handle);
 
 export { mapsRoutes };
