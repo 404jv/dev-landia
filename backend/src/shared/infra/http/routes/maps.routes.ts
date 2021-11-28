1e657007ef9d5e4aeaa6abb2ea17ea62373093b9
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-11-28 14:41:12 -0300

    Add ensureAuthenticated to route create map

--- backend/src/shared/infra/http/routes/maps.routes.ts
+++ backend/src/shared/infra/http/routes/maps.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { CreateMapController } from '@modules/maps/useCases/CreateMap/CreateMapController';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const mapsRoutes = Router();
 
 const createMapController = new CreateMapController();
 
-mapsRoutes.post('/create', createMapController.handle);
+mapsRoutes.post('/create', ensureAuthenticated, createMapController.handle);
 
 export { mapsRoutes };
