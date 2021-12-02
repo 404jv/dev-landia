dc6842acc2c2aedbffc50d749c539bb87f76604f
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-01 22:14:58 -0300

    Create migration to create table phases

---
+++ backend/src/shared/infra/typeorm/migrations/1638406652432-create-table-phases.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createTablePhases1638406652432 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'phases',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'map_id',
+            type: 'uuid',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'markdown_text',
+            type: 'text',
+          },
+          {
+            name: 'max_level',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKMapPhase',
+            referencedTableName: 'maps',
+            referencedColumnNames: ['id'],
+            columnNames: ['map_id'],
+            onDelete: 'SET NULL',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('phases');
+  }
+}
