a19c70887ad83934f45c388161a75bfb71132ed8
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-12 19:30:15 -0300

    throw error if a non-existent activity id is provided

--- backend/src/modules/game/useCases/AddActivitiesToPhase/AddActivitiesToPhaseUseCase.spec.ts
+++ backend/src/modules/game/useCases/AddActivitiesToPhase/AddActivitiesToPhaseUseCase.spec.ts
@@ -1,32 +1,60 @@
 import { IAddActivitiesToPhaseDTO } from '@modules/game/dtos/IAddActivitiesToPhaseDTO';
+import { InMemoryActivityRepository } from '@modules/game/repositories/in-memory/InMemoryActivityRepository';
 import { InMemoryPhaseActivitiesRepository } from '@modules/game/repositories/in-memory/InMemoryPhaseActivitiesRepository';
 import { InMemoryPhasesRepository } from '@modules/game/repositories/in-memory/InMemoryPhasesRepository';
 
 import { AddActivitiesToPhaseUseCase } from './AddActivitiesToPhaseUseCase';
+import { ActivityNotFoundError } from './errors/ActivityNotFoundError';
 import { PhaseNotFoundError } from './errors/PhaseNotFoundError';
 
 let inMemoryPhaseActivitiesRepository: InMemoryPhaseActivitiesRepository;
 let inMemoryPhasesRepository: InMemoryPhasesRepository;
+let inMemoryActivitiesRepository: InMemoryActivityRepository;
 let addActivitiesToPhaseUseCase: AddActivitiesToPhaseUseCase;
+
+enum enType {
+  THEORY = 'theory',
+  PRACTICE = 'practice',
+}
 
 describe('Add Activities to Phase', () => {
   beforeEach(() => {
     inMemoryPhaseActivitiesRepository = new InMemoryPhaseActivitiesRepository();
     inMemoryPhasesRepository = new InMemoryPhasesRepository();
+    inMemoryActivitiesRepository = new InMemoryActivityRepository();
     addActivitiesToPhaseUseCase = new AddActivitiesToPhaseUseCase(
       inMemoryPhaseActivitiesRepository,
-      inMemoryPhasesRepository
+      inMemoryPhasesRepository,
+      inMemoryActivitiesRepository
     );
   });
 
   it('should not be able to add activities to a non-existent phase', async () => {
-    const phase: IAddActivitiesToPhaseDTO = {
+    const activitiesToPhase: IAddActivitiesToPhaseDTO = {
       activities_ids: ['1234'],
       phase_id: 'non-existent-id',
     };
 
-    await expect(addActivitiesToPhaseUseCase.execute(phase)).rejects.toEqual(
-      new PhaseNotFoundError()
-    );
+    await expect(
+      addActivitiesToPhaseUseCase.execute(activitiesToPhase)
+    ).rejects.toEqual(new PhaseNotFoundError());
+  });
+
+  it('should not be able to add activities to a non-existent phase', async () => {
+    const phase = await inMemoryPhasesRepository.create({
+      map_id: '123',
+      title: 'Fase teste',
+      max_level: 5,
+      type: enType.PRACTICE,
+    });
+
+    const activitiesToPhase: IAddActivitiesToPhaseDTO = {
+      activities_ids: ['non-existent-id'],
+      phase_id: phase.id,
+    };
+
+    await expect(
+      addActivitiesToPhaseUseCase.execute(activitiesToPhase)
+    ).rejects.toEqual(new ActivityNotFoundError('non-existent-id'));
   });
 });
--- backend/src/modules/game/useCases/AddActivitiesToPhase/AddActivitiesToPhaseUseCase.ts
+++ backend/src/modules/game/useCases/AddActivitiesToPhase/AddActivitiesToPhaseUseCase.ts
@@ -1,13 +1,16 @@
 import { IAddActivitiesToPhaseDTO } from '@modules/game/dtos/IAddActivitiesToPhaseDTO';
+import { IActivitiesRepository } from '@modules/game/repositories/IActivitiesRepository';
 import { IPhaseActivitiesRepository } from '@modules/game/repositories/IPhaseActivitiesRepository';
 import { IPhasesRepository } from '@modules/game/repositories/IPhasesRepository';
 
+import { ActivityNotFoundError } from './errors/ActivityNotFoundError';
 import { PhaseNotFoundError } from './errors/PhaseNotFoundError';
 
 class AddActivitiesToPhaseUseCase {
   constructor(
     private phaseActivitiesRepository: IPhaseActivitiesRepository,
-    private phasesRepository: IPhasesRepository
+    private phasesRepository: IPhasesRepository,
+    private activitiesRepository: IActivitiesRepository
   ) {}
 
   async execute({
@@ -20,9 +23,31 @@ class AddActivitiesToPhaseUseCase {
       throw new PhaseNotFoundError();
     }
 
+    const nonExistentActivityId = await this.findNonExistentActivityById(
+      activities_ids
+    );
+
+    if (nonExistentActivityId) {
+      throw new ActivityNotFoundError(nonExistentActivityId);
+    }
+
     activities_ids.map(async (activity_id) => {
       await this.phaseActivitiesRepository.create(phase_id, activity_id);
     });
+  }
+
+  private async findNonExistentActivityById(
+    activities_ids: string[]
+  ): Promise<string> {
+    const nonExistentId = activities_ids.find(async (activity_id) => {
+      const activityExists = await this.activitiesRepository.findById(
+        activity_id
+      );
+
+      return activityExists;
+    });
+
+    return nonExistentId;
   }
 }
 
