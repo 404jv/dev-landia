cb659941f1390e9779d4b2ae6151d8fc8bf9da35
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-14 17:46:32 -0300

    Add routes to addActivitiesToPhaseController

--- backend/src/shared/infra/http/routes/phases.routes.ts
+++ backend/src/shared/infra/http/routes/phases.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { AddActivitiesToPhaseController } from '@modules/game/useCases/AddActivitiesToPhase/AddActivitiesToPhaseController';
 import { CreatePhaseController } from '@modules/game/useCases/CreatePhase/CreatePhaseController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
@@ -8,8 +9,14 @@ import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 const phasesRoutes = Router();
 
 const createPhaseController = new CreatePhaseController();
+const addActivitiesToPhaseController = new AddActivitiesToPhaseController();
 
 phasesRoutes.use(ensureAuthenticated);
 phasesRoutes.post('/create', ensureAdmin, createPhaseController.handle);
+phasesRoutes.post(
+  '/add-activities',
+  ensureAdmin,
+  addActivitiesToPhaseController.handle
+);
 
 export { phasesRoutes };
