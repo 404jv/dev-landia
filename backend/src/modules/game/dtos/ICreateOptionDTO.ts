5128661e326f56f6c25099aa5a2118b17b3712ca
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-16 20:00:49 -0300

    Create ICreateOptionDTO

---
+++ backend/src/modules/game/dtos/ICreateOptionDTO.ts
@@ -0,0 +1,14 @@
+enum enOptionType {
+  JS_FUNCTION = 'js_function',
+  COMMAND = 'command',
+}
+
+interface ICreateOptionDTO {
+  id?: string;
+  name: string;
+  activity_id: string;
+  type: enOptionType;
+  hexadecimal_color: string;
+}
+
+export { ICreateOptionDTO };
