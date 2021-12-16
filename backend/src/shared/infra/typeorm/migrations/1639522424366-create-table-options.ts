28e7b5d12517a3e1332342b45a9c77c215b4aac7
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-16 20:01:03 -0300

    Create migration to create options table

---
+++ backend/src/shared/infra/typeorm/migrations/1639522424366-create-table-options.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createTableOptions1639522424366 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'options',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'activity_id',
+            type: 'uuid',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            enum: ['js_function', 'command'],
+          },
+          {
+            name: 'hexadecimal_colar',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKActivityOption',
+            referencedTableName: 'activities',
+            referencedColumnNames: ['id'],
+            columnNames: ['activity_id'],
+            onDelete: 'CASCADE',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('options');
+  }
+}
