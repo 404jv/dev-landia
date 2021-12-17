b9c4b10c2b28c8f67c5415daf6bf46e4263fb5c4
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-17 19:13:51 -0300

    add middleware ensureAuthenticated and ensureAdmin to create activity route

--- backend/src/shared/infra/http/routes/activity.routes.ts
+++ backend/src/shared/infra/http/routes/activity.routes.ts
@@ -2,10 +2,14 @@ import { Router } from 'express';
 
 import { CreateActivityController } from '@modules/game/useCases/CreateActivity/CreateActivityController';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const createActivityController = new CreateActivityController();
 
 const activityRoutes = Router();
 
-activityRoutes.post('/create', createActivityController.handle);
+activityRoutes.use(ensureAuthenticated);
+activityRoutes.post('/create', ensureAdmin, createActivityController.handle);
 
 export { activityRoutes };
