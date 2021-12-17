41665d6f5f042e05c5eedb6c8716235aa843a1da
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-17 17:59:45 -0300

    add singleton to OptionsRepository

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -4,10 +4,12 @@ import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/Us
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { ActivitiesRepository } from '@modules/game/infra/typeorm/repositories/ActivitiesRepository';
 import { MapsRepository } from '@modules/game/infra/typeorm/repositories/MapsRepository';
+import { OptionsRepository } from '@modules/game/infra/typeorm/repositories/OptionsRepository';
 import { PhaseActivitiesRepository } from '@modules/game/infra/typeorm/repositories/PhaseActivitiesRepository';
 import { PhasesRepository } from '@modules/game/infra/typeorm/repositories/PhasesRepository';
 import { IActivitiesRepository } from '@modules/game/repositories/IActivitiesRepository';
 import { IMapsRepository } from '@modules/game/repositories/IMapsRepository';
+import { IOptionsRepository } from '@modules/game/repositories/IOptionsRepository';
 import { IPhaseActivitiesRepository } from '@modules/game/repositories/IPhaseActivitiesRepository';
 import { IPhasesRepository } from '@modules/game/repositories/IPhasesRepository';
 
@@ -31,4 +33,9 @@ container.registerSingleton<IActivitiesRepository>(
 container.registerSingleton<IPhaseActivitiesRepository>(
   'PhaseActivitiesRepository',
   PhaseActivitiesRepository
+);
+
+container.registerSingleton<IOptionsRepository>(
+  'OptionsRepository',
+  OptionsRepository
 );
