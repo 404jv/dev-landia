a4fe4ac29bffdaa40ce111c4774f2332c07a77e6
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-17 18:15:22 -0300

    Remove PhaseActivity repository and entity

--- backend/src/modules/game/repositories/IPhaseActivitiesRepository.ts
+++
@@ -1,7 +0,0 @@
-import { PhaseActivity } from '../infra/typeorm/entities/PhaseActivity';
-
-interface IPhaseActivitiesRepository {
-  create(phase_id: string, activity_id: string): Promise<PhaseActivity>;
-}
-
-export { IPhaseActivitiesRepository };
