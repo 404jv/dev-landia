fb05db5c633667b39c15a6e186dd0456227ab0bf
João Victor R. Alves <joaovictorramalho7@gmail.com>
2021-12-28 18:20:15 -0300

    Return is_done to all maps

--- backend/src/modules/game/useCases/ListTree/ListTreeUseCase.ts
+++ backend/src/modules/game/useCases/ListTree/ListTreeUseCase.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 import { Map } from '@modules/game/infra/typeorm/entities/Map';
 import { Phase } from '@modules/game/infra/typeorm/entities/Phase';
 import { IMapsRepository } from '@modules/game/repositories/IMapsRepository';
+import { IUsersMapsRepository } from '@modules/game/repositories/IUsersMapsRepository';
 import { IUsersPhasesRepository } from '@modules/game/repositories/IUsersPhasesRepository';
 
 @injectable()
@@ -11,17 +12,28 @@ class ListTreeUseCase {
     @inject('MapsRepository')
     private mapsRepository: IMapsRepository,
     @inject('UsersPhases')
-    private usersPhases: IUsersPhasesRepository
+    private usersPhases: IUsersPhasesRepository,
+    @inject('UsersMaps')
+    private usersMaps: IUsersMapsRepository
   ) {}
 
   async execute(user_id: string): Promise<Map[]> {
-    const maps = await this.mapsRepository.list(user_id);
+    const maps = await this.mapsRepository.list();
 
     const tree = await Promise.all(
       maps.map(async (map) => {
-        const phasesWithLevel = await this.getPhasesLevel(map.phases, user_id);
+        const { is_done } = await this.usersMaps.findByUserAndMap(
+          user_id,
+          map.id
+        );
 
+        Object.assign(map, {
+          is_done,
+        });
+
+        const phasesWithLevel = await this.getPhasesLevel(map.phases, user_id);
         map.phases = phasesWithLevel;
+
         return map;
       })
     );
