9e4d56255e12e26b253a615628b0912fdfb21b92
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-01-13 21:56:34 -0300

    Create seeds to maps

---
+++ backend/src/shared/infra/typeorm/seed/maps.ts
@@ -0,0 +1,32 @@
+import { createConnection } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+async function create() {
+  const connection = await createConnection();
+
+  await connection.query(`
+    INSERT INTO 
+      maps(id, title, description, "order")
+    VALUES (
+      '2fad788d-e552-4232-ab72-ff4aa3ef6378',
+      'mapa 2',
+      'mapa dois',
+      2
+    );
+  `);
+
+  await connection.query(`
+    INSERT INTO 
+      maps(id, title, description, "order")
+    VALUES (
+      '654b369c-8459-4c7e-be93-b74531c7f8de',
+      'mapa 1',
+      'mapa um',
+      1
+    );
+  `);
+
+  await connection.close();
+}
+
+create();
