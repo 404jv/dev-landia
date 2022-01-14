f08bf764c353fbcad161d0e74c6e19c449cc2bed
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-01-13 21:56:24 -0300

    get connection from typeorm

--- backend/src/shared/infra/typeorm/seed/admin.ts
+++ backend/src/shared/infra/typeorm/seed/admin.ts
@@ -1,7 +1,6 @@
 import { hash } from 'bcrypt';
+import { createConnection } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
-
-import createConnection from '../index';
 
 async function create() {
   const connection = await createConnection();
@@ -26,6 +25,4 @@ async function create() {
   await connection.close();
 }
 
-create()
-  .then(() => console.log('User admin created!'))
-  .catch((err) => console.log(err));
+create();
