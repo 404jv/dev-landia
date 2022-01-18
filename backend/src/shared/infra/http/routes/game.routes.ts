b780bd2baab9ccc5c437f0af292bc6a0182976e3
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-01-17 21:33:28 -0300

    Route to getPhase

--- backend/src/shared/infra/http/routes/game.routes.ts
+++ backend/src/shared/infra/http/routes/game.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { GetPhaseController } from '@modules/game/useCases/getPhase/GetPhaseController';
 import { ListTreeController } from '@modules/game/useCases/ListTree/ListTreeController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
@@ -7,8 +8,10 @@ import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 const gameRoutes = Router();
 
 const listTreeController = new ListTreeController();
+const getPhaseController = new GetPhaseController();
 
 gameRoutes.use(ensureAuthenticated);
 gameRoutes.get('/tree', listTreeController.handle);
+gameRoutes.get('/get-phase', getPhaseController.handle);
 
 export { gameRoutes };
