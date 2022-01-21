87dda2ab0734931fb9af9469e830339fb2a6b99d
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-01-21 11:14:11 -0300

    Add activity_answer as optional to ICreateActivityDTO

--- backend/src/modules/game/dtos/ICreateActivityDTO.ts
+++ backend/src/modules/game/dtos/ICreateActivityDTO.ts
@@ -14,6 +14,7 @@ interface ICreateActivityDTO {
   options?: ICreateOptionDTO[];
   order: number;
   created_at?: Date;
+  activity_answer?: ICreateOptionDTO[];
 }
 
 export { ICreateActivityDTO };
