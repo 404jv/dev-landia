4f10eb35a8eee0a3e931d03272480d0b447bfb20
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-01-22 19:39:53 -0300

    Create IAddOptionsToActivityDTO

---
+++ backend/src/modules/game/dtos/IAddOptionsToActivityDTO.ts
@@ -0,0 +1,7 @@
+interface IAddOptionsToActivityDTO {
+  activity_id: string;
+  activityDefaultCodeOptionsIds: string[];
+  activityAnswerOptionsIds: string[];
+}
+
+export { IAddOptionsToActivityDTO };
