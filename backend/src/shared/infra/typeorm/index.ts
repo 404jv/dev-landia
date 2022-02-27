4780324cbb8022a0a16534f35207c7dc15c07d94
João <joao@pop-os.localdomain>
2022-02-26 22:05:53 -0300

    Refactor

--- backend/src/shared/infra/typeorm/index.ts
+++ backend/src/shared/infra/typeorm/index.ts
@@ -1,12 +1,15 @@
 import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 
-export default async (host = 'database'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOption = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOption, {
-      host: 'database',
-      database: 'devlandia',
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : 'database',
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'devlandia_test'
+          : defaultOption.database,
     })
   );
 };
