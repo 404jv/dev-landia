8c60c2996b9955b57952097bc540a9a4d626d0cc
Joao Victor R. Alves <joaovictorramalho7@gmail.com>
2022-03-02 19:40:33 -0300

    Fix get phase

--- backend/src/modules/phases/useCases/getPhase/GetPhaseUseCase.ts
+++ backend/src/modules/phases/useCases/getPhase/GetPhaseUseCase.ts
@@ -1,26 +1,52 @@
 import { inject, injectable } from 'tsyringe';
 
 import { Activity } from '@modules/activities/infra/typeorm/entities/Activity';
+import { IActivitiesOptionsRepository } from '@modules/activities/repositories/IActivitiesOptionsRepository';
 import { IPhasesRepository } from '@modules/phases/repositories/IPhasesRepository';
 
 @injectable()
 class GetPhaseUseCase {
   constructor(
     @inject('PhasesRepository')
-    private phasesRepository: IPhasesRepository
+    private phasesRepository: IPhasesRepository,
+    @inject('ActivitiesAnswersRepository')
+    private activitiesAnswersRepository: IActivitiesOptionsRepository,
+    @inject('ActivitiesDefaultCodeRepository')
+    private activitiesDefaultCodeRepository: IActivitiesOptionsRepository
   ) {}
 
   async execute(phase_id: string, phaseLevel: number): Promise<Activity[]> {
     const activitiesEnd = phaseLevel * 5 - 1;
     const activitiesStart = activitiesEnd - 4;
 
-    const phase = await this.phasesRepository.findAndSelectActivities(
+    const { activities } = await this.phasesRepository.findAndSelectActivities(
       phase_id,
       activitiesStart,
       activitiesEnd
     );
 
-    return phase.activities;
+    const activitiesWithAnswer = await Promise.all(
+      activities.map(async (activity) => {
+        const activity_answer =
+          await this.activitiesAnswersRepository.findOptionsByActivityId(
+            activity.id
+          );
+
+        const default_code =
+          await this.activitiesDefaultCodeRepository.findOptionsByActivityId(
+            activity.id
+          );
+
+        Object.assign(activity, {
+          activity_answer,
+          default_code,
+        });
+
+        return activity;
+      })
+    );
+
+    return activitiesWithAnswer;
   }
 }
 
