c8fb2ab674023306520113c3cbc1edf339ea87a9
Joao Victor R. Alves <joaovictorramalho7@gmail.com>
2022-03-06 12:51:14 -0300

    Create GetPhaseController.spec.ts

---
+++ backend/src/modules/phases/useCases/getPhase/GetPhaseController.spec.ts
@@ -0,0 +1,52 @@
+import request from 'supertest';
+import { Connection } from 'typeorm';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+import { createMap } from '@test/factories/MapFactory';
+import { createPhaseAndActivities } from '@test/factories/PhaseFactory';
+import { createUserAndAuthenticate } from '@test/factories/UserFactory';
+
+let connection: Connection;
+let phase_id: string;
+let userToken: string;
+
+describe('Get Phase Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const map = await createMap();
+
+    const phase = await createPhaseAndActivities(map.id);
+    phase_id = phase.id;
+
+    const userJwt = await createUserAndAuthenticate();
+    userToken = userJwt.token;
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('Should be able to a user get their phase', async () => {
+    const response = await request(app)
+      .get('/game/get-phase')
+      .send({
+        phase_id,
+        phaseLevel: 0,
+      })
+      .set('Authorization', `Bearer ${userToken}`);
+
+    console.log(response.body);
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveLength(5);
+  });
+});
+
+/*
+  - Retornar 5 atividade
+
+*/
