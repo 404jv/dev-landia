c8f51881fa493e1c71b1408877587c418e591d85
Joao Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-03-18 18:24:35 -0300

    Create route to GeTheoryPhase

--- backend/src/shared/infra/http/routes/game.routes.ts
+++ backend/src/shared/infra/http/routes/game.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { ListTreeController } from '@modules/game/useCases/ListTree/ListTreeController';
 import { GetPracticePhaseController } from '@modules/phases/useCases/GetPracticePhase/GetPracticePhaseController';
+import { GetTheoryPhaseController } from '@modules/phases/useCases/GetTheoryPhase/GetTheoryPhaseController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -9,6 +10,7 @@ const gameRoutes = Router();
 
 const listTreeController = new ListTreeController();
 const getPracticePhaseController = new GetPracticePhaseController();
+const getTheoryPhaseController = new GetTheoryPhaseController();
 
 gameRoutes.use(ensureAuthenticated);
 gameRoutes.get('/tree', listTreeController.handle);
@@ -16,6 +18,11 @@ gameRoutes.get(
   '/practice-phase/:id',
   ensureAuthenticated,
   getPracticePhaseController.handle
+);
+gameRoutes.get(
+  '/theory-phase/:id',
+  ensureAuthenticated,
+  getTheoryPhaseController.handle
 );
 
 export { gameRoutes };
