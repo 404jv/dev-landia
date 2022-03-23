401ab26ffb8c473b6e1f7190b75bb9691d0446ae
Joao Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-03-23 18:32:19 -0300

    Create function startUserPhaseAtLevel2 in GameFactory

--- backend/src/test/factories/GameFactory.ts
+++ backend/src/test/factories/GameFactory.ts
@@ -16,3 +16,16 @@ export async function startUserPhase(
   const usersPhasesRepository = new UsersPhasesRepository();
   await usersPhasesRepository.create(user_id, phase_id);
 }
+
+export async function startUserPhaseAtLevel2(
+  user_id: string,
+  phase_id: string
+): Promise<void> {
+  const usersPhasesRepository = new UsersPhasesRepository();
+
+  const userPhase = await usersPhasesRepository.create(user_id, phase_id);
+
+  userPhase.current_level = 2;
+
+  await usersPhasesRepository.update(userPhase);
+}
