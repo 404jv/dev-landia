4c4773a271c08dead6b98a6d6efa61daa175dc88
1SyuLi <ruan_goio10@hotmail.com>
2022-04-07 19:40:32 -0300

    :scroll: Home refactor

--- mobile/src/Global/styles/theme.ts
+++ mobile/src/Global/styles/theme.ts
@@ -1,33 +1,33 @@
 export default {
   colors: {
-    background: '#0C0D0D',
-    primary: '#111212',
-    secondary: '#181B1D',
-    stroke: '#2F3437',
-    black: '#000000',
+    background: "#0C0D0D",
+    primary: "#111212",
+    secondary: "#181B1D",
+    stroke: "#2F3437",
+    black: "#000000",
 
-    white: '#FFFFFF',
-    title: '#EDEDED',
-    subTitle: '#9FA2A5',
-    description: '#C9C9C9',
+    white: "#FFFFFF",
+    title: "#EDEDED",
+    subTitle: "#9FA2A5",
+    description: "#C9C9C9",
 
-    black_title: '#47474D',
-    black_subTitle: '#6E6E70',
+    black_title: "#47474D",
+    black_subTitle: "#6E6E70",
 
-    text_detail: '#AEAEB3',
+    text_detail: "#AEAEB3",
 
-    blue: '#1F6FEB',
-    lightBlue: '#425675',
+    blue: "#1F6FEB",
+    lightBlue: "#425675",
 
     bash: {
-      blue: '#0055A4',
-      red: '#EF4135',
-      white: '#FFFFFF',
-    }
+      blue: "#0055A4",
+      red: "#EF4135",
+      white: "#FFFFFF",
+    },
   },
   fonts: {
-    regular: 'Roboto_400Regular',
-    medium: 'Roboto_500Medium',
-    bold: 'Roboto_700Bold',
-  }
-}
+    regular: "Roboto_400Regular",
+    medium: "Roboto_500Medium",
+    bold: "Roboto_700Bold",
+  },
+};
