abeeb4280a2a1ccd8742889b8cff4e3303b09603
João Vitor Lima <jvolima2004@gmail.com>
2022-04-08 09:19:49 -0300

    Página Home e componente Card

--- mobile/src/pages/Home/Card/index.tsx
+++ mobile/src/pages/Home/Card/index.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 
-import { CardIcon, Container, Percentage, CardInfo } from "./styles";
+import { CardIcon, Container, Percentage, CardInfo, CardTexts, Title, Description, ContainerInfos, ProgressBar, ContainerProgressBar } from "./styles";
 
-export function Card(): JSX.Element {
+interface CardProps {
+  name: string;
+  description: string;
+  percentage: number;
+}
+
+export function Card({ name, description, percentage }: CardProps): JSX.Element {
   return (
     <Container>
-      <CardInfo>
-        <CardIcon name="play-circle" />
-        <Percentage>50%</Percentage>
-      </CardInfo>
+      <ContainerInfos>
+        <CardInfo>
+          <CardIcon name="play-circle" percentage={percentage}/>
+          <Percentage percentage={percentage}>{percentage}%</Percentage>
+        </CardInfo>
+        <CardTexts>
+          <Title>{name}</Title>
+          <Description>
+            {description}
+          </Description>
+        </CardTexts>
+      </ContainerInfos>
+      <ContainerProgressBar>
+        <ProgressBar percentage={percentage} />
+      </ContainerProgressBar>
     </Container>
   );
 }
