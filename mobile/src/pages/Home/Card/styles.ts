7e25881b6306634e9d0bd5656ded8db6bf9041da
Joao Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-04-12 18:44:24 -0300

    Eslint fix

--- mobile/src/pages/Home/Card/styles.ts
+++ mobile/src/pages/Home/Card/styles.ts
@@ -1,18 +1,18 @@
 import styled from "styled-components/native";
 import { Feather } from "@expo/vector-icons";
-import theme from "../../../Global/styles/theme";
+import globalTheme from "../../../Global/styles/theme";
 
 type CardIconProps = {
   percentage: number;
-}
+};
 
 type PercentageProps = {
   percentage: number;
-}
+};
 
 type ProgressBarProps = {
   percentage: number;
-}
+};
 
 export const Container = styled.View`
   width: 100%;
@@ -35,14 +35,20 @@ export const CardInfo = styled.View`
 
 export const CardIcon = styled(Feather)<CardIconProps>`
   font-size: 60px;
-  color: ${({ percentage }) => percentage == 100 ? theme.colors.progressBar : theme.colors.title};
+  color: ${({ percentage }) =>
+    percentage === 100
+      ? globalTheme.colors.progressBar
+      : globalTheme.colors.title};
   margin-bottom: 6px;
 `;
 
 export const Percentage = styled.Text<PercentageProps>`
   font-size: 18px;
   font-family: ${({ theme }) => theme.fonts.medium};
-  color: ${({ percentage }) => percentage == 100 ? theme.colors.progressBar : theme.colors.title};
+  color: ${({ percentage }) =>
+    percentage === 100
+      ? globalTheme.colors.progressBar
+      : globalTheme.colors.title};
 `;
 
 export const CardTexts = styled.View`
@@ -50,21 +56,21 @@ export const CardTexts = styled.View`
   width: 100%;
   flex-direction: column;
   margin-top: 6px;
-  margin-left: 30px
-`
+  margin-left: 30px;
+`;
 
 export const Title = styled.Text`
   font-size: 24px;
   color: ${({ theme }) => theme.colors.white};
-  font-weight: bold
-`
+  font-weight: bold;
+`;
 
 export const Description = styled.Text`
   font-size: 14px;
   font-weight: bold;
   margin-top: 10px;
   color: ${({ theme }) => theme.colors.description};
-`
+`;
 
 export const ContainerProgressBar = styled.View`
   margin-top: -14px;
@@ -72,11 +78,14 @@ export const ContainerProgressBar = styled.View`
   height: 14px;
   background-color: ${({ theme }) => theme.colors.stroke};
   border-radius: 12px;
-`
+`;
 
 export const ProgressBar = styled.View<ProgressBarProps>`
   width: ${({ percentage }) => percentage}%;
   height: 14px;
-  background-color: ${({ percentage }) => percentage == 100 ? theme.colors.progressBar : theme.colors.blue};
+  background-color: ${({ percentage }) =>
+    percentage === 100
+      ? globalTheme.colors.progressBar
+      : globalTheme.colors.blue};
   border-radius: 12px;
-`
+`;
