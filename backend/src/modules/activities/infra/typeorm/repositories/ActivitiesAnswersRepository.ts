fac5eab9be09e59b9c20b0e2ef60613e5500a18e
Joao Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-04-20 23:08:05 -0300

    Fix: when add options to an activity the others options are deleted

--- backend/src/modules/activities/infra/typeorm/repositories/ActivitiesAnswersRepository.ts
+++ backend/src/modules/activities/infra/typeorm/repositories/ActivitiesAnswersRepository.ts
@@ -41,11 +41,7 @@ class ActivitiesAnswersRepository implements IActivitiesOptionsRepository {
   }
 
   async deleteAllByActivityId(activity_id: string): Promise<void> {
-    await this.repository
-      .createQueryBuilder('activity_answer')
-      .delete()
-      .where('activity_answer.activity_id = activity_id', { activity_id })
-      .execute();
+    await this.repository.delete({ activity_id });
   }
 }
 
