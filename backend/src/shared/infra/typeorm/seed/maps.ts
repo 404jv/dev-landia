3b7c70bbb167fb5ae055701dc6fa88fb1cf158b1
João Vitor Lima <jvolima2004@gmail.com>
2022-04-21 14:59:55 -0300

    arrumando as seeds

--- backend/src/shared/infra/typeorm/seed/maps.ts
+++ backend/src/shared/infra/typeorm/seed/maps.ts
@@ -7,6 +7,17 @@ async function create() {
     INSERT INTO 
       maps(id, title, description, "order")
     VALUES (
+      '2fad788d-e552-4232-ab72-ff4aa3ef6378',
+      'mapa 2',
+      'mapa dois',
+      2
+    );
+  `);
+
+  await connection.query(`
+    INSERT INTO 
+      maps(id, title, description, "order")
+    VALUES (
       '654b369c-8459-4c7e-be93-b74531c7f8de',
       'mapa 1',
       'mapa um',
