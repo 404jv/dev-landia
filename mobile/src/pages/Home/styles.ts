6cf7e965d45713524a4e661a74375558fd90f50a
Joao Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-04-23 22:07:12 -0300

    Change container View to ScrollView

--- mobile/src/pages/Home/styles.ts
+++ mobile/src/pages/Home/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
 `;
