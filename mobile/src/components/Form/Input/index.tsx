e5306857b60e1fd9e3ba7c2c52c6a6087339f3f2
João Vitor Lima <jvolima2004@gmail.com>
2022-05-03 19:01:42 -0300

    arrumando problema do NaN na home e invertendo sentido dos mapas

--- mobile/src/components/Form/Input/index.tsx
+++ mobile/src/components/Form/Input/index.tsx
@@ -1,51 +1,48 @@
-import React, { useState } from 'react';
-import { Feather } from '@expo/vector-icons';
-import { useTheme } from 'styled-components';
-import { TextInputProps } from 'react-native';
+import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
+import { TextInputProps } from "react-native";
 
-import {
-    Container,
-    InputText,
-    IconContainer,
-} from './styles';
+import { Container, InputText, IconContainer } from "./styles";
 
 interface InputProps extends TextInputProps {
-    iconName: React.ComponentProps<typeof Feather>['name'];
-    value?: string;
+  iconName: React.ComponentProps<typeof Feather>["name"];
+  value?: string;
 }
 
-export function Input({ iconName, value, ...rest }: InputProps) {
+export function Input({ iconName, value, ...rest }: InputProps): JSX.Element {
+  const theme = useTheme();
 
-    const theme = useTheme();
+  const [isFocused, setIsfocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
 
-    const [isFocused, setIsfocused] = useState(false);
-    const [isFilled, setIsFilled] = useState(false);
+  function handleInputFocus(): void {
+    setIsfocused(true);
+  }
 
-    function handleInputFocus() {
-        setIsfocused(true);
-    }
+  function handleInputBlur(): void {
+    setIsfocused(false);
+    setIsFilled(!!value);
+  }
 
-    function handleInputBlur() {
-        setIsfocused(false);
-        setIsFilled(!!value);
-    }
+  return (
+    <Container>
+      <IconContainer isFocused={isFocused}>
+        <Feather
+          name={iconName}
+          size={26}
+          color={
+            isFocused || isFilled ? theme.colors.blue : theme.colors.text_detail
+          }
+        />
+      </IconContainer>
 
-    return (
-        <Container>
-            <IconContainer isFocused={isFocused}>
-                <Feather
-                    name={iconName}
-                    size={26}
-                    color={(isFocused || isFilled) ? theme.colors.blue : theme.colors.text_detail}
-                />
-            </IconContainer>
-
-            <InputText
-                isFocused={isFocused}
-                onFocus={handleInputFocus}
-                onBlur={handleInputBlur}
-                {...rest}
-            />
-        </Container>
-    );
+      <InputText
+        isFocused={isFocused}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        {...rest}
+      />
+    </Container>
+  );
 }
