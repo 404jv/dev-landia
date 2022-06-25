b0e94339bf8507eeed47015b5f3960c72cd66d2f
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-06-25 19:10:49 -0300

    fix eslint

--- mobile/src/components/Form/PasswordInput/index.tsx
+++ mobile/src/components/Form/PasswordInput/index.tsx
@@ -1,71 +1,70 @@
-import React, { useState } from 'react';
-import { Feather } from '@expo/vector-icons';
-import { useTheme } from 'styled-components';
-import { TextInputProps } from 'react-native';
+import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
+import { TextInputProps } from "react-native";
 
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from "react-native";
 
-import {
-    Container,
-    InputText,
-    IconContainer,
-} from './styles';
+import { Container, InputText, IconContainer } from "./styles";
 
 interface InputProps extends TextInputProps {
-    iconName: React.ComponentProps<typeof Feather>['name'];
-    value?: string;
+  iconName: React.ComponentProps<typeof Feather>["name"];
+  value?: string;
 }
 
-export function PasswordInput({ iconName, value, ...rest }: InputProps) {
+export function PasswordInput({
+  iconName,
+  value,
+  ...rest
+}: InputProps): JSX.Element {
+  const theme = useTheme();
+  const [revealPassword, setRevealPassword] = useState(true);
 
-    const theme = useTheme();
-    const [revealPassword, setRevealPassword] = useState(true);
+  function handleRevealPassword(): void {
+    setRevealPassword(!revealPassword);
+  }
 
-    function handleRevealPassword() {
-        setRevealPassword(!revealPassword);
-    }
+  const [isFocused, setIsfocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
 
-    const [isFocused, setIsfocused] = useState(false);
-    const [isFilled, setIsFilled] = useState(false);
+  function handleInputFocus(): void {
+    setIsfocused(true);
+  }
 
-    function handleInputFocus() {
-        setIsfocused(true);
-    }
+  function handleInputBlur(): void {
+    setIsfocused(false);
+    setIsFilled(!!value);
+  }
 
-    function handleInputBlur() {
-        setIsfocused(false);
-        setIsFilled(!!value);
-    }
+  return (
+    <Container>
+      <IconContainer isFocused={isFocused}>
+        <Feather
+          name={iconName}
+          size={26}
+          color={
+            isFocused || isFilled ? theme.colors.blue : theme.colors.text_detail
+          }
+        />
+      </IconContainer>
 
+      <InputText
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        secureTextEntry={revealPassword}
+        isFocused={isFocused}
+        {...rest}
+      />
 
-
-    return (
-        <Container>
-            <IconContainer isFocused={isFocused}>
-                <Feather
-                    name={iconName}
-                    size={26}
-                    color={(isFocused || isFilled) ? theme.colors.blue : theme.colors.text_detail}
-                />
-            </IconContainer>
-
-            <InputText
-                onFocus={handleInputFocus}
-                onBlur={handleInputBlur}
-                secureTextEntry={revealPassword}
-                isFocused={isFocused}
-                {...rest}
-            />
-
-            <IconContainer isFocused={isFocused}>
-                <TouchableOpacity onPress={handleRevealPassword}>
-                    <Feather
-                        name={revealPassword ? 'eye' : 'eye-off'}
-                        size={26}
-                        color={theme.colors.text_detail}
-                    />
-                </TouchableOpacity>
-            </IconContainer>
-        </Container>
-    );
+      <IconContainer isFocused={isFocused}>
+        <TouchableOpacity onPress={handleRevealPassword}>
+          <Feather
+            name={revealPassword ? "eye" : "eye-off"}
+            size={26}
+            color={theme.colors.text_detail}
+          />
+        </TouchableOpacity>
+      </IconContainer>
+    </Container>
+  );
 }
--- mobile/src/components/Form/PasswordInput/styles.ts
+++ mobile/src/components/Form/PasswordInput/styles.ts
@@ -1,41 +1,45 @@
-import styled, { css } from 'styled-components/native';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { TextInput } from 'react-native';
+import styled, { css } from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { TextInput } from "react-native";
 
 interface Props {
-    isFocused?: boolean;
-};
+  isFocused?: boolean;
+}
 
 export const Container = styled.View`
-   width: 100%;
-   height: 54px;
-   background-color: ${({ theme }) => theme.colors.title};
-   flex-direction: row;
-   margin-bottom: 14px;
+  width: 100%;
+  height: 54px;
+  background-color: ${({ theme }) => theme.colors.title};
+  flex-direction: row;
+  margin-bottom: 14px;
 `;
 
 export const IconContainer = styled.View<Props>`
-    height: 54px;
-    width: 54px;
-    background-color: ${({ theme }) => theme.colors.title};
-    align-items: center;
-    justify-content: center;
+  height: 54px;
+  width: 54px;
+  background-color: ${({ theme }) => theme.colors.title};
+  align-items: center;
+  justify-content: center;
 
-    ${({ isFocused, theme }) => isFocused && css`
-        border-bottom-width: 2px;
-        border-bottom-color: ${theme.colors.blue};
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.blue};
     `}
 `;
 
-export const InputText = styled(TextInput) <Props>`
-    flex: 1;
-    background-color: ${({ theme }) => theme.colors.title};
-    color: ${({ theme }) => theme.colors.text_detail};
-    font-size: ${RFValue(18)}px;
-    padding: 0px 10px;
+export const InputText = styled(TextInput)<Props>`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.title};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(18)}px;
+  padding: 0px 10px;
 
-    ${({ isFocused, theme }) => isFocused && css`
-        border-bottom-width: 2px;
-        border-bottom-color: ${theme.colors.blue};
+  ${({ isFocused, theme }) =>
+    isFocused &&
+    css`
+      border-bottom-width: 2px;
+      border-bottom-color: ${theme.colors.blue};
     `}
 `;
