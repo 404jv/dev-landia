7eeb3b6bf9f0cd43aa1eb745200ed241638714ee
João Vitor Lima <jvolima2004@gmail.com>
2022-09-29 09:19:33 -0300

    Rota de listar todos os maps do banco

--- backend/src/shared/infra/http/routes/maps.routes.ts
+++ backend/src/shared/infra/http/routes/maps.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateMapController } from '@modules/maps/useCases/CreateMap/CreateMapController';
+import { ListMapsController } from '@modules/maps/useCases/ListMaps/ListMapsController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
@@ -8,8 +9,11 @@ import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 const mapsRoutes = Router();
 
 const createMapController = new CreateMapController();
+const listMapsController = new ListMapsController();
 mapsRoutes.use(ensureAuthenticated);
 
 mapsRoutes.post('/create', ensureAdmin, createMapController.handle);
+
+mapsRoutes.get('/', ensureAdmin, listMapsController.handle);
 
 export { mapsRoutes };
